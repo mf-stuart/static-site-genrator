12ff41d45afe46ccd4e8cfaf25151dbf183306b8
mf-stuart <MatthewZinkiewiczID@outlook.com>
2025-06-02 19:17:51 +0100

    CH3 -> L4 Complete

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
-
-from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter
+from textnode import (TextNode, TextType, text_node_to_html_node,
+                      split_nodes_delimiter, extract_markdown_images, extract_markdown_links,
+                      split_nodes_image, split_nodes_link)
 
 
 class TestTextNode(unittest.TestCase):
@@ -36,8 +37,120 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.value, "This is a link node")
         self.assertEqual(html_node.props, {"href": "https://www.boot.dev"})
 
-    def test_split_nodes_delimiter(self):
-        print()
+    def test_split_nodes_delimiter_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        print(split_nodes_delimiter([node], "`", TextType.CODE))
+        self.assertEqual(split_nodes_delimiter([node], "`", TextType.CODE),
+                         [TextNode("This is text with a " , TextType.TEXT),
+                          TextNode("code block", TextType.CODE),
+                         TextNode(" word", TextType.TEXT)])
+
+    def test_split_nodes_delimiter_italic(self):
+        node = TextNode("This is text with a _italic_ word", TextType.TEXT)
+        print(split_nodes_delimiter([node], "_", TextType.ITALIC))
+        self.assertEqual(split_nodes_delimiter([node], "_", TextType.ITALIC),
+                         [TextNode("This is text with a ", TextType.TEXT),
+                          TextNode("italic", TextType.ITALIC),
+                          TextNode(" word", TextType.TEXT)])
+
+    def test_split_nodes_delimiter_mix(self):
+        node = TextNode("This is text with _italic_ and **bold** words", TextType.TEXT)
+        first_pass = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        second_pass = split_nodes_delimiter(first_pass, "**", TextType.BOLD)
+        print(second_pass)
+        self.assertEqual(second_pass,
+                         [
+                             TextNode("This is text with ", TextType.TEXT),
+                             TextNode("italic", TextType.ITALIC),
+                             TextNode(" and ", TextType.TEXT),
+                             TextNode("bold", TextType.BOLD),
+                             TextNode(" words", TextType.TEXT),
+                         ])
+
+    def test_split_nodes_delimiter_single(self):
+        node = TextNode("**bold**", TextType.TEXT)
+        self.assertEqual(split_nodes_delimiter([node], "**", TextType.BOLD),
+                         [TextNode("bold", TextType.BOLD),])
+
+    def test_split_nodes_delimiter_multi(self):
+        node = TextNode("**bold**_italic_", TextType.TEXT)
+        first_pass = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        second_pass = split_nodes_delimiter(first_pass, "**", TextType.BOLD)
+        self.assertEqual(second_pass,
+                         [
+                             TextNode("bold", TextType.BOLD),
+                             TextNode("italic", TextType.ITALIC)
+                         ])
+
+    def test_split_nodes_delimiter_many_nodes(self):
+        node1 = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a **bold text** node", TextType.TEXT)
+        node3 = TextNode("This is another node", TextType.TEXT)
+        node4 = TextNode("This is a _italic text_ node", TextType.TEXT)
+        node5 = TextNode("This is a _italic_ and **bold** node", TextType.TEXT)
+
+        old_nodes = [node1, node2, node3, node4, node5]
+        firstpass = split_nodes_delimiter(old_nodes, "**", TextType.BOLD)
+        secondpass = split_nodes_delimiter(firstpass, "_", TextType.ITALIC)
+
+        self.assertListEqual(
+            secondpass,
+            [TextNode("This is a text node", TextType.TEXT, None),
+            TextNode("This is a ", TextType.TEXT, None),
+            TextNode("bold text", TextType.BOLD, None),
+            TextNode(" node", TextType.TEXT, None),
+            TextNode("This is another node", TextType.TEXT, None),
+            TextNode("This is a ", TextType.TEXT, None),
+            TextNode("italic text", TextType.ITALIC, None),
+            TextNode(" node", TextType.TEXT, None),
+            TextNode("This is a ", TextType.TEXT, None),
+            TextNode("italic", TextType.ITALIC, None),
+            TextNode(" and ", TextType.TEXT, None),
+            TextNode("bold", TextType.BOLD, None),
+            TextNode(" node", TextType.TEXT, None)]
+            )
+
+
+    def test_extract_markdown_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual(extract_markdown_images(text), [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")])
+
+    def test_extract_markdown_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        self.assertEqual(extract_markdown_links(text), [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")])
+
+    def test_split_nodes_image(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode("second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"),
+            ],
+            new_nodes
+        )
+
+    def test_split_nodes_link(self):
+        node = TextNode(
+            "This is text with [google](https://google.com) and [youtube](https://www.youtube.com)]!",
+            TextType.TEXT
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with ", TextType.TEXT),
+                TextNode("google", TextType.TEXT, "https://google.com"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("youtube", TextType.TEXT, "https://www.youtube.com/@google"),
+                TextNode("!", TextType.TEXT),
+            ],
+            new_nodes
+        )
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from leafnode import LeafNode
+import re
 
 class TextType(Enum):
     TEXT = "text"
@@ -8,8 +9,6 @@ class TextType(Enum):
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
-
-
 
 class TextNode():
     def __init__(self, text, text_type, url=None) :
@@ -49,5 +48,47 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     for old_node in old_nodes:
         text_slices = old_node.text.split(delimiter)
         for i, text_slice in enumerate(text_slices):
-            new_nodes.append(TextNode(text_slice, TextType.TEXT if i % 2 == 0 else text_type))
+            if text_slice:
+                new_nodes.append(TextNode(text_slice, old_node.text_type if i % 2 == 0 else text_type))
     return new_nodes
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    image_delimiter =r"(!\[[^\[\]]*\]\([^\(\)]*\))"
+    for node in old_nodes:
+        text_slices = re.split(image_delimiter, node.text)
+        for text_slice in text_slices:
+            if text_slice:
+                if props := extract_markdown_images(text_slice):
+                    new_nodes.append(TextNode(props[0][0],
+                                              TextType.IMAGE,
+                                              props[0][1]))
+                else:
+                    new_nodes.append(TextNode(text_slice, node.text_type))
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    link_delimiter = r"(\[[^\[\]]*\]\([^\(\)]*\))"
+    for node in old_nodes:
+        text_slices = re.split(link_delimiter, node.text)
+        for text_slice in text_slices:
+            if text_slice:
+                if props := extract_markdown_links(text_slice):
+                    new_nodes.append(TextNode(props[0][0],
+                                              TextType.IMAGE,
+                                              props[0][1]))
+                else:
+                    new_nodes.append(TextNode(text_slice, node.text_type))
+            return new_nodes
+
+def extract_markdown_images(text):
+    image_mask =r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    return re.findall(image_mask, text)
+
+def extract_markdown_links(text):
+    link_mask = r"\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    return re.findall(link_mask, text)
+
+
